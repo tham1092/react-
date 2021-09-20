6e039ef09d45b57f00cf3ec5b7b1b4f00e4c4305
Thamara Ramos <thamararamos@MacBook-Air-de-Thamara.local>
2021-09-20 14:36:43 -0300

    archivo listo

---
+++ src/views/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Hola Dojo!</h1>
+                <h2>Cosas que necesito aprender:</h2>
+                <ul>
+                    <li>Mucho más acerca de React</li>
+                    <li>Extends</li>
+                    <li>Components</li>
+                    <li>Props</li>
+                </ul>
+            </div>
+        )
+    }
+}
+export default Home;
